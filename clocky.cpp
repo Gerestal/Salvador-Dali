2f17eb5d5493925bddce6035737af2c866c103cc
HowToKartoxa <anton@ozhigin.com>
2025-07-07 14:16:57 +0300

    added basic clock functionality

---
+++ clocky.cpp
@@ -0,0 +1,26 @@
+#include "ui_clocky.h"
+#include "clocky.h"
+
+Clocky::Clocky(QWidget* parent)
+    : updateTimer(new QTimer(this))
+{
+    updateTimer->setInterval(50);
+}
+
+void Clocky::startClock(QTime time)
+{
+    currentTime = time;
+    useRealTime = false;
+    updateTimer->start();
+}
+
+void Clocky::startClock()
+{
+    currentTime = QTime::currentTime();
+    useRealTime = true;
+    updateTimer->start();
+}
+
+void Clocky::stopClock(){
+    updateTimer->stop();
+}
