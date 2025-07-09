3be32eeb7788dd93b029c33e75c76fd8ff68c9a4
Staniskhan <andromedas82000@gmail.com>
2025-07-09 21:27:47 +0300

    rename ui-form of mainwindow. connect signal of formatchislovoy and mainwindow and clocky

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "ui_mainwindow1.h"
 #include <QMessageBox>
 #include <QShortcut>
 
@@ -59,7 +59,7 @@ void MainWindow::from_device_triggered()
 void MainWindow::chislovoy_input_triggered()
 {
     formatchislovoy = new FormatChislovoy;
-    // connect (formatchislovoy, &FormatChislovoy::start_clock_signal, this, &MainWindow::push_start_time_date);
+    connect (formatchislovoy, &FormatChislovoy::start_clock_signal, this, &MainWindow::push_start_time_date);
 }
 
 void MainWindow::slovestny_input_triggered()
@@ -99,8 +99,7 @@ void MainWindow::exit_triggered()
     this->~MainWindow();
 }
 
-void MainWindow::push_start_time_date(/*...*/)
+void MainWindow::push_start_time_date(const QTime &time)
 {
-    // startDate = formatchislovoy->returnDate();
-    // startTime = formatchislovoy->returnTime();
+    clock->startClock(time);
 }
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -32,6 +32,8 @@ public:
     QAction *manual_action;
     QAction *show_statistics_action;
     QAction *end_testing_action;
+    QAction *fullscreen_action;
+    QAction *exit_action;
     QWidget *centralwidget;
     QGridLayout *gridLayout;
     Clocky *widget;
@@ -62,6 +64,10 @@ public:
         show_statistics_action->setObjectName("show_statistics_action");
         end_testing_action = new QAction(MainWindow);
         end_testing_action->setObjectName("end_testing_action");
+        fullscreen_action = new QAction(MainWindow);
+        fullscreen_action->setObjectName("fullscreen_action");
+        exit_action = new QAction(MainWindow);
+        exit_action->setObjectName("exit_action");
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         gridLayout = new QGridLayout(centralwidget);
@@ -95,6 +101,8 @@ public:
         menubar->addAction(format_menu->menuAction());
         menubar->addAction(help_menu->menuAction());
         file_menu->addAction(about_action);
+        file_menu->addAction(fullscreen_action);
+        file_menu->addAction(exit_action);
         statistics_menu->addAction(show_statistics_action);
         statistics_menu->addAction(end_testing_action);
         format_menu->addAction(digf_menu->menuAction());
@@ -118,6 +126,8 @@ public:
         manual_action->setText(QCoreApplication::translate("MainWindow", "\320\222\321\200\321\203\321\207\320\275\321\203\321\216", nullptr));
         show_statistics_action->setText(QCoreApplication::translate("MainWindow", "\320\237\321\200\320\276\321\201\320\274\320\276\321\202\321\200\320\265\321\202\321\214 \321\201\321\202\320\260\321\202\320\270\321\201\321\202\320\270\320\272\321\203", nullptr));
         end_testing_action->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\272\320\276\320\275\321\207\320\270\321\202\321\214 \321\202\320\265\321\201\321\202\320\270\321\200\320\276\320\262\320\260\320\275\320\270\320\265", nullptr));
+        fullscreen_action->setText(QCoreApplication::translate("MainWindow", "\320\237\320\276\320\273\320\275\320\276\321\215\320\272\321\200\320\260\320\275\320\275\321\213\320\271 \321\200\320\265\320\266\320\270\320\274 (F11)", nullptr));
+        exit_action->setText(QCoreApplication::translate("MainWindow", "\320\222\321\213\320\271\321\202\320\270", nullptr));
         file_menu->setTitle(QCoreApplication::translate("MainWindow", "\320\244\320\260\320\271\320\273", nullptr));
         statistics_menu->setTitle(QCoreApplication::translate("MainWindow", "\320\241\321\202\320\260\321\202\320\270\321\201\321\202\320\270\320\272\320\260", nullptr));
         format_menu->setTitle(QCoreApplication::translate("MainWindow", "\320\244\320\276\321\200\320\274\320\260\321\202", nullptr));
